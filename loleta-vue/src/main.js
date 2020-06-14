ec95c8529a1a5eb58e0174eed4a910a17c700b15
Lucas <lucasmanuelperez@gmail.com>
2020-06-14 01:00:02 -0300

    Se agrega Vuetify

--- loleta-vue/src/main.js
+++ loleta-vue/src/main.js
@@ -2,11 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from './plugins/vuetify';
+import '@babel/polyfill'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
+
+Vue.use(VueAxios, axios)
+axios.defaults.baseURL = 'http://localhost:3000/api';
 
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
